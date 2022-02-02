10b6c4f8addc74c56c6eb0ffa88f3d907cfcd4bd
vilmajal <vilma.jalava@gmail.com>
2022-02-02 11:44:46 +0100

    LIC8 implementation and LIC7 comments fix #9

--- src/main/java/com/example/CMV.java
+++ src/main/java/com/example/CMV.java
@@ -63,6 +63,10 @@ public class CMV {
     private Boolean LIC6() {
         return false;
     }
+
+    /**
+     * @return true if there exists a set of two datapoints separated by K_PTS with distance > length1
+     */
     private Boolean LIC7() {
         List<Coordinate> points = param.getPOINTS();
         int k_pts = param.getK_PTS();
@@ -84,9 +88,39 @@ public class CMV {
         return false;
     }
 
+    /**
+     * @return true if there exists three datapoints separated by A_PTS and B_PTSs that cannot be contained within a circle of radius1
+     * returns false if there are less than 5 NUMPOINTS or datapoints can be contained within circle.
+     */
     private Boolean LIC8() {
-        return false;
+        List<Coordinate> points = param.getPOINTS();
+        int radius1 = param.getRADIUS1();
+        int numpoints = param.getNUMPOINTS();
+        int a_points = param.getA_PTS();
+        int b_points = param.getB_PTS();
+        if(numpoints<5){
+            return false;
+        }
+        if(radius1<=0){
+            return false;
+        }
+        for(int i =0; i < points.size()-a_points-b_points-2; i++){
+            int Ax = points.get(i).getX();
+            int Ay = points.get(i).getY();
+            int Bx = points.get(i+a_points+1).getX();
+            int By = points.get(i+a_points+1).getY();
+            int Cx = points.get(i+a_points+b_points+2).getX();
+            int Cy = points.get(i+a_points+b_points+2).getY();
+            double AB = getDistance(Ax,Ay,Bx,By);
+            double BC = getDistance(Bx,By,Cx,Cy);
+            double CA = getDistance(Cx,Cy,Ax,Ay);
+            if(AB <= radius1*2 && BC <= radius1*2 && CA <= radius1*2){
+                return false;
+            }
+        }
+        return true;
     }
+
     private Boolean LIC9() {
         return false;
     }
