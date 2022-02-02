46d9598edbc70d29208ac30ec7804ee3dbb40549
SebaztianOjebrant <98548302+SebaztianOjebrant@users.noreply.github.com>
2022-02-02 09:31:48 +0100

    Fixed issue: Altered test cases to fit the codefix

--- src/test/java/com/example/CMVTest.java
+++ src/test/java/com/example/CMVTest.java
@@ -100,6 +100,7 @@ public class CMVTest{
 	@Test
     public void lic13_one_true(){
 		points.add(new Coordinate(0,0));
+	    	points.add(new Coordinate(0,1));
 		points.add(new Coordinate(0,2));
 		points.add(new Coordinate(1,1)); 
 		points.add(new Coordinate(2,1)); 
@@ -125,11 +126,12 @@ public class CMVTest{
 		points.add(new Coordinate(0,0));
 		points.add(new Coordinate(0,2));
 		points.add(new Coordinate(1,1)); 
-		points.add(new Coordinate(2,1)); 
+		points.add(new Coordinate(1,2));
+		points.add(new Coordinate(2,1));
 		points.add(new Coordinate(2,2));
-		points.add(new Coordinate(3,1)); 
+		points.add(new Coordinate(3,1));  
 		points.add(new Coordinate(3,2));
-		points.add(new Coordinate(4,3));
+		points.add(new Coordinate(4,3)); 
 		
 		param = new Parameters(points.size(), points);
 		
@@ -165,6 +167,7 @@ public class CMVTest{
 	@Test
     public void lic14_one_true(){
 		points.add(new Coordinate(0,0));
+	    	points.add(new Coordinate(0,1));
 		points.add(new Coordinate(0,2));
 		points.add(new Coordinate(1,1)); 
 		points.add(new Coordinate(2,1)); 
@@ -188,6 +191,7 @@ public class CMVTest{
 	@Test
     public void lic14_both_true(){
 		points.add(new Coordinate(0,0));
+	    	points.add(new Coordinate(0,1));
 		points.add(new Coordinate(0,2));
 		points.add(new Coordinate(1,1)); 
 		points.add(new Coordinate(2,1)); 
